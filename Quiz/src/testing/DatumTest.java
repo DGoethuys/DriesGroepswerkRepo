aea10c79421419832097a13d4b8a799165b1caaf
Dries Goethuys <dries.goethuys@gmail.com>
2013-10-06 15:18:06 +0200

    Testklasse toegevoegd

---
+++ Quiz/src/testing/DatumTest.java
@@ -0,0 +1,48 @@
+package testing;
+
+import static org.junit.Assert.*;
+//Als ik deze imports als code laat zijn er fouten, als ik "utils"
+//maak van "org.java" niet, maar dan komt er dat ze nog niet worden
+//gebruikt dus heb ik ze even als documentatie gezet tegen foutmeldingen
+//import org.java.Datum;
+//import org.java.Gregoriaans;
+
+import org.junit.Test;
+
+public class DatumTest {
+
+	@Test
+	public void Test_SetDatum_GeldigeParameters_Aanvaard() {
+		
+	}
+	@Test
+	public void Test_SetDag_GeldigeDagen_Aanvaard() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetMaand_GeldigeMaanden_Aanvaard() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetJaar_GeldigeJaren_Aanvaard() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetDatum_Exception_OngeldigeParameters() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetDag_Exception_OngeldigAantalDagen() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetMaand_Exception_OngeldigAantalMaanden() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void Test_SetJaar_Exception_OngeldigJaar() {
+		fail("Not yet implemented");
+	}
+	
+
+}
