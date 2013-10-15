5cd02db68063e9f9421924bda43edb74b9a7bbc7
java <java@ubuntu>
2013-10-15 22:05:55 +0200

    Meerkeuze

---
+++ Quiz/src/model/Meerkeuze.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Meerkeuze extends Opdracht{
+
+	private String vraag;
+	private String juisteAntwoord;
+	private int aantalPogingen;
+	private ArrayList<String> Keuze = new ArrayList<String>();
+	private int maxAntwoordTijd;
+	private String keuze;
+	
+	
+	public Meerkeuze() {
+	 for (String s : Keuze)
+		{
+		    keuze += s + "\n";
+		}
+			
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Meerkeuze(String vraag, String juisteAntwoord, int aantalPogingen, int maxAntwoordTijd, ArrayList<String> Keuze){
+		this.setVraag(vraag);
+		this.setJuisteAntwoord(juisteAntwoord);
+		this.setAantalPogingen(aantalPogingen);
+		this.setMaxAntwoordTijd(maxAntwoordTijd);
+		this.setKeuze(Keuze);
+	}
+	
+	public void setKeuze(ArrayList<String> Keuze ){
+		this.Keuze = Keuze;
+	}
+	
+	@Override
+	public String toString() {
+		return "Opdracht - vraag: " + this.vraag + ", juiste antwoord: "
+				+ this.juisteAntwoord + ", aantal pogingen: " + this.aantalPogingen
+				+ ", maximale antwoord tijd: " + this.maxAntwoordTijd + ", Keuzes: " + this.keuze;
+	}
+
+}
