5dc8369ead80504dbae2968770f7043a5ee94ee7
Spartagon <Jan_Haepers@hotmail.com>
2013-12-09 19:29:52 +0100

    CommitEnumLeraren

--- Quiz/src/Enums/EnumLeraren.java
+++ Quiz/src/Enums/EnumLeraren.java
@@ -4,6 +4,19 @@ public class EnumLeraren {
 	public enum Leraren{
 		Jan,Nicolas,Dries,Glenn,Sofie
 	}
+
+	public static Object enumsToStringArray() {
+		// TODO Auto-generated method stub
+		
+		String[] results = new String[Leraren.values().length];  
+        int count = 0;  
+        for (Leraren each  : Leraren.values()) {  
+            results[count] = each.toString();  
+            count++;  
+        }  
+        return results;
+		
+	}
 	
 
 }
