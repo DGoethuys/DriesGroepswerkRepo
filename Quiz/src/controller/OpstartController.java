e8613df5836e27d36d111f9ac6be7176acd4ba10
DGoethuys <dries.goethuys@gmail.com>
2013-12-21 19:19:57 +0100

    PersistentieFacade aangepast naar singleton, CategorieOpdrachtCollections aangepast hiernaar als voorbeeld, OpstartController hiernaar aangepast en PersistentieType weg gedaan en de method daaruit genomen en in PersistentieFacade gestoken

--- Quiz/src/controller/OpstartController.java
+++ Quiz/src/controller/OpstartController.java
@@ -8,16 +8,13 @@ import java.util.Scanner;
 
 import model.Opdracht;
 import persistentie.PersistentieFacade;
-import persistentie.PersistentieType;
 import views.Menu;
 import views.MainWindow;
 
 public class OpstartController {
 	
 	public PersistentieFacade getPersistentie(){
-		PersistentieType type = new PersistentieType();
-		PersistentieFacade persistentie = type.getPersistentie(setPersistentieFromInit());
-		persistentie.laadData();
+		PersistentieFacade persistentie = PersistentieFacade.getPersistentie();
 		return persistentie;
 	}
 	
@@ -47,42 +44,5 @@ public class OpstartController {
 	}
 
 	
-	public String setPersistentieFromInit()throws NullPointerException{
-		String persistentie = null;
-		try{
-			persistentie = this.leesVanInit("Persistentie");
-			if(persistentie == null){
-				throw new NullPointerException("Geen persistentie gevonden in init bestand");
-			}
-		}
-		catch(Exception ex){
-			System.out.println("Property niet gevonden");
-		}
-		return persistentie;
-	}
 	
-	private String leesVanInit(String p){
-		String property = null;
-		File file = new File("bestanden/init.txt");
-		try{
-			Scanner scanner = new Scanner(file);
-			while (scanner.hasNext() && !scanner.hasNext("END")){
-				String lijn = scanner.nextLine();
-				String [] velden = lijn.split(">");
-				if(velden[0].contains(p)){
-					property = velden[1];
-					}
-				}
-			if (scanner!=null){
-				scanner.close();
-				}
-			}
-		catch(FileNotFoundException ex){
-			System.out.println("init bestand niet gevonden");
-			}
-		catch(Exception ex){
-			System.out.println("Error: " + ex.getMessage());
-			}
-		return property;
-	}// Einde method
 }// Einde class
--- Quiz/src/persistentie/PersistentieFacade.java
+++ Quiz/src/persistentie/PersistentieFacade.java
@@ -1,21 +1,34 @@
 package persistentie;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import model.OpdrachtCatalogus;
 import model.QuizCatalogus;
 import model.QuizOpdrachtCatalogus;
 
-//Hier in worden de namen van de methods gedeclareerd die we zullen gebruiken voor data te laden
-//en op te slaan in zowel de database als de textbestanden
-//Catalogussen staan hier in zodat deze in allebij de persistenties kunnen gebruikt worden zonder
-//ze appart te declareren en initialiseren
+//Singleton class die een object aanmaakt waar alle catalogussen in zitten
 
 public abstract class PersistentieFacade {
+	
+	//static instance of PersistentieFacade
+	private static PersistentieFacade persistentie;
+	
 	protected OpdrachtCatalogus oc = new OpdrachtCatalogus();
 	protected QuizCatalogus qc = new QuizCatalogus();
 	protected QuizOpdrachtCatalogus qoc = new QuizOpdrachtCatalogus();
 	
-	public abstract void laadData();
+	//private constructor
+	protected PersistentieFacade(){
+		laadData();
+	}
+	
+	//Abstract method om data te lezen
+	protected abstract void laadData();
+	//Abstract method om data weg te schrijven
 	public abstract void bewaarData();
+	
 	public abstract String getPersistentieType();
 	
 	public OpdrachtCatalogus getOpdrachtCatalogus(){
@@ -29,5 +42,60 @@ public abstract class PersistentieFacade {
 	public QuizOpdrachtCatalogus getQuizOpdrachtCatalogus(){
 		return qoc;
 	}
+	
+	// Geeft een persistentie voor een type terug
+  	public static PersistentieFacade getPersistentie() {
+  		String persistentieType = setPersistentieFromInit();
+  		if(persistentie == null){
+  	  	  	if (persistentieType.equals("Text")){
+  	  	  		persistentie = new TextPersistentie();
+  	  	  	}
+  			else if(persistentieType.equals("Database")){
+  				persistentie = new DataBasePersistentie();
+  			}
+  		}
+  		return persistentie;
+  	}// Einde method
+  	
+  	//Gebruikt de method leesVanInit om een String met het type van persistentie terug te geven
+  	private static String setPersistentieFromInit()throws NullPointerException{
+		String persistentieType = null;
+		try{
+			persistentieType = leesVanInit("Persistentie");
+			if(persistentieType == null){
+				throw new NullPointerException("Geen persistentie gevonden in init bestand");
+			}
+		}
+		catch(Exception ex){
+			System.out.println("Property niet gevonden");
+		}
+		return persistentieType;
+	}// Einde method
+	
+  	//Leest uit het init bestand welk type persistenitie gebruikt moet worden
+	private static String leesVanInit(String p){
+		String property = null;
+		File file = new File("bestanden/init.txt");
+		try{
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNext() && !scanner.hasNext("END")){
+				String lijn = scanner.nextLine();
+				String [] velden = lijn.split(">");
+				if(velden[0].contains(p)){
+					property = velden[1];
+					}
+				}
+			if (scanner!=null){
+				scanner.close();
+				}
+			}
+		catch(FileNotFoundException ex){
+			System.out.println("init bestand niet gevonden");
+			}
+		catch(Exception ex){
+			System.out.println("Error: " + ex.getMessage());
+			}
+		return property;
+	}// Einde method
 	
 }// Einde class
